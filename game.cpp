2b719b32a8b642ef260a568baf5ef4db33d4ca0e
Muaazzzzz <165038637+Muaazzzzz@users.noreply.github.com>
2025-02-04 20:43:16 +0100

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -25,3 +25,23 @@ void playerMove(char board[3][3], int player) {
         }
     }
 }
+
+bool checkWin(char board[3][3]) {
+    for (int i = 0; i < 3; ++i) {
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') return true;
+        if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ') return true;
+    }
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') return true;
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') return true;
+
+    return false;
+}
+
+bool checkDraw(char board[3][3]) {
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            if (board[i][j] == ' ') return false;
+        }
+    }
+    return true;
+}
