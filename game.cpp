a69711cb2f2f023387daec46a857afad71e969c1
OpenHe1mer42 <138040685+OpenHe1mer42@users.noreply.github.com>
2025-02-13 13:45:13 +0100

    Fixed input error
    Sometimes the user can enter a character as an input. We fixed that edge case scenario. Added a new library called limit

--- game.cpp
+++ game.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 using namespace std;
 
 void initializeBoard(char board[3][3]) {
@@ -9,18 +10,34 @@ void initializeBoard(char board[3][3]) {
     }
 }
 
+
 void playerMove(char board[3][3], int player) {
     int row, col;
     char mark = (player == 1) ? 'X' : 'O';
-
+    
     while (true) {
         cout << "Player " << player << " (" << mark << "), enter your move (row and column: 1-3): ";
-        cin >> row >> col;
+        cout << "Row: ";
+        if (!(cin >> row)) {
+            cout << "Invalid input: Please enter a valid integer for row." << endl;
+            cin.clear(); 
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+            continue;
+        }
+        
+        cout << "Col: ";
+        if (!(cin >> col)) {
+            cout << "Invalid input: Please enter a valid integer for column." << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            continue;
+        }
         
         if (row >= 1 && row <= 3 && col >= 1 && col <= 3 && board[row-1][col-1] == ' ') {
             board[row-1][col-1] = mark;
             break;
-        } else {
+        }
+        else {
             cout << "Invalid move, try again." << endl;
         }
     }
